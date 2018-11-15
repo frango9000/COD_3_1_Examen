4dcbecde35c1e64d6ccdadc6cdd404eb257732e6
Francisco Sanchez <fsancheztemprano@danielcastelao.org>
2018-11-15 09:12:24 +0100

    2) Cambio de nombre de variables variable f rafactorizada a var numF variable j rafactorizada a var numJ variable i rafactorizada a var numI

--- Factorial/Factorial.java
+++ Factorial/Factorial.java
@@ -4,22 +4,22 @@ public class Factorial {
 
     public static void main(String[] args) {
 
-        int j;
-        int f;
+        int numJ;
+        int numF;
 
-        j = 8;
+        numJ = 8;
 
-        int i;
-        if (j == 0) {
-            f = 1;
+        int numI;
+        if (numJ == 0) {
+            numF = 1;
         } else {
-            f = 1;
-            for (i = j; i >= 1; i--) {
-                f = f * i;
+            numF = 1;
+            for (numI = numJ; numI >= 1; numI--) {
+                numF = numF * numI;
             }
         }
 
-        System.out.println(f);
+        System.out.println(numF);
 
     }
 
